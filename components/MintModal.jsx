51ab6f92443f67a7eb00c7b0dab04c6af1eaa469
harshpandey002 <coding.harshp@gmail.com>
2022-10-21 17:59:59 +0530

    NFT Mint Modal

--- components/MintModal.jsx
+++ components/MintModal.jsx
@@ -1,7 +1,8 @@
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import styles from "@/styles/MintModal.module.css";
 import ReactDOM from "react-dom";
-import { AiOutlineClose } from "react-icons/ai";
+import { AiOutlineClose, AiOutlineCloudUpload } from "react-icons/ai";
+import { useDropzone } from "react-dropzone";
 
 export default function MintModal({ show, onClose }) {
   const [isBrowser, setIsBrowser] = useState(false);
@@ -10,6 +11,16 @@ export default function MintModal({ show, onClose }) {
     name: "",
     email: "",
     password: "",
+  });
+
+  const onDrop = useCallback((acceptedFiles) => {
+    handleDrop(acceptedFiles);
+  }, []);
+
+  const { getRootProps, getInputProps, isDragAccept, open } = useDropzone({
+    onDrop,
+    accept: { "image/*": [] },
+    maxFiles: 1,
   });
 
   useEffect(() => {
@@ -25,10 +36,25 @@ export default function MintModal({ show, onClose }) {
     onClose();
   };
 
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+  };
+
+  const handleDrop = (acceptedFiles) => {
+    const reader = new FileReader();
+    reader.readAsDataURL(acceptedFiles[0]);
+
+    reader.onabort = () => console.log("file reading was aborted");
+    reader.onerror = () => console.log("file reading has failed");
+    reader.onload = () => {
+      const imageUri = reader.result;
+      console.log(imageUri);
+    };
+  };
+
   const modalContent = show ? (
     <>
       <div className="overlay" onClick={handleClose} />
-
       <div className={styles.modal}>
         <div className={styles.header}>
           <span className={styles.title}>
@@ -41,18 +67,31 @@ export default function MintModal({ show, onClose }) {
           </span>
         </div>
         <h3 id={styles.meta}>Metadata</h3>
-        <form className={styles.form}>
+        <form onSubmit={handleSubmit} className={styles.form}>
           <div className="input-group">
             <label htmlFor="token">Name</label>
             <input id="token" type="text" />
           </div>
           <div className="input-group">
             <label htmlFor="description">Description</label>
-            <textarea rows={5} id="description" />
+            <textarea rows={3} id="description" />
+          </div>
+          <div className={styles.imageSection}>
+            <div
+              style={{ borderColor: isDragAccept ? "#134cae" : "" }}
+              className={styles.dropzone}
+              {...getRootProps()}
+            >
+              <input {...getInputProps()} />
+
+              <AiOutlineCloudUpload id={styles.uploadIcon} />
+              <p>Upload Image</p>
+            </div>
+            <button onClick={open}>Select File</button>
           </div>
           <div className={styles.cta}>
             <button>Close</button>
-            <button>Mint NFT</button>
+            <button type="submit">Mint NFT</button>
           </div>
         </form>
       </div>
