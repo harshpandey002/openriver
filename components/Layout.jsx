0a3442f37aca3ac89dd577e72c9f35eeb448dc93
Harsh Pandey <coding.harshp@gmail.com>
2022-10-29 11:31:00 +0530

    Favicon

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -31,6 +31,24 @@ const Layout = ({ title, description, children }) => {
         <title>{title}</title>
         <meta name="title" content={title} />
         <meta name="description" content={description} />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest"></link>
       </Head>
 
       {isMismatched && (
